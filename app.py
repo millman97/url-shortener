e9d8b145f7f1e7be48976b55e2370240fded0c05
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-12 17:08:17 +0100

    add placeholder controllers, Links model, update /api route methods

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ from werkzeug.exceptions import BadRequest, NotFound, InternalServerError
 app = Flask(__name__)
 CORS(app)
 
+
+
+
+
+
+
+
+
+## Routes *****************************************************************************************
 # Catch All Redirect to Index.html
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
@@ -17,11 +26,18 @@ def redirect_to_link():
     pass
 # need handling for incorrect id
 
-@app.route('/api',  methods=['Get', 'Post'])
+@app.route('/api', methods=['Get', 'Post'], title='All Links')
 def all_links():
+    fns = {"GET": index, "POST": create}
+    if request.method == "POST":
+        "link": request.form["link"]
+        pass
+    if request.method == "GET":
+        resp, code = fns[request.method](request)
+        return jsonify(resp), code
     pass
 
-@app.route('/api/<page_id>',  methods=['Get', 'Delete'])
+@app.route('/api/<page_id>',  methods=['Get', 'Delete'], title='Link:<page_id>')
 def link_by_id():
     pass
 
@@ -36,3 +52,32 @@ def handle_405(err):
 @app.errorhandler(InternalServerError)
 def handle_500(err):
     return render_template('errors/500.html', title='Oops!'), 500
+
+
+
+
+
+## Model ******************************************************************************************
+class Links(db.Model):
+    __tablename__ = 'links'
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    link = db.Column(db.String())
+
+    def __init__(self,link):
+        self.link = link
+    
+    def __repr__(self):
+        return f"{self.id} - {self.name}"
+
+
+
+
+
+
+## Controllers ************************************************************************************
+def index():
+    pass
+
+def create():
+    pass
