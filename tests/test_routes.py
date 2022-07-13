ba70e5504d78967b3ea610643e0e38afbbda4491
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-13 17:28:05 +0100

    add route tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,13 +1,26 @@
 import json
+import random
+import string
 
 def test_home(api):
-    """Home Page Loads"""
+    """Test Home Page Loads"""
     resp = api.get('/')
     assert resp.status == '200 OK'
     assert b'Enter the URL you would like to shorten!' in resp.data
     
 def test_links(api):
-    """Home Page Loads"""
-    resp = api.get('/', method='GET')
+    """Test Return All Links Route"""
+    resp = api.get('/link', method='GET')
     assert resp.status == '200 OK'
+    assert b'link' in resp.data
     assert len(resp.data)>1
+
+def test_generate_link(api):
+    """Test Create Link Route"""
+    random_seqeuence = string.ascii_lowercase + string.digits
+    random_string = random.choices(random_seqeuence, k=6)
+    random_string = "".join(random_string)
+    resp = api.post('/link', data={"link": random_string})
+    print(resp.status)
+    assert resp.status == '200 OK'
+    assert b'Shortened' in resp.data
