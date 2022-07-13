056e355b9082a90cfe6d172f01b339095186ce89
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-13 16:59:18 +0100

    add another test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -5,3 +5,9 @@ def test_home(api):
     resp = api.get('/')
     assert resp.status == '200 OK'
     assert b'Enter the URL you would like to shorten!' in resp.data
+    
+def test_links(api):
+    """Home Page Loads"""
+    resp = api.get('/', method='GET')
+    assert resp.status == '200 OK'
+    assert len(resp.data)>1
