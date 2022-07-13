2e840d346cab9bcc0d7f8ecfe505156b000f8f78
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-13 15:29:49 +0100

    remove old script refs

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-from app import routes, database, controllers, models, clipboard
+from app import routes, database, controllers, models 
 
 if __name__ == "__main__":
     app.run(debug=True)
